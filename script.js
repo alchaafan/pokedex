01c14d834221aba2ca6c8a01a687eb0a1ab83b60
Muhammad <m.shaafan@outlook.de>
2025-05-06 15:33:24 +0200

    overlay

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-let initArray = [];
 let loadOffset = 0;
 
 async function fetchData() {
-    let pokemon = await fetch (`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${loadOffset}`);
-    let data = await pokemon.json();
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${loadOffset}`);
+    let data = await response.json();
     let pokemonList = data.results;
 
-    for (let i = 0; i < pokemonList.length; i++ ) {
-        let pokemonDetails = await fetch(pokemonList[i].url);
-        let pokemonData = await pokemonDetails.json();
-
-        renderPokemon(pokemonData)
+    for (let i = 0; i < pokemonList.length; i++) {
+        let detailsResponse = await fetch(pokemonList[i].url);
+        let pokemonData = await detailsResponse.json();
+        renderPokemon(pokemonData);
     }
 
+    loadOffset += 25;
 }
-
 
 function renderPokemon(pokemon) {
     const contentRef = document.getElementById('content');
-    contentRef.innerHTML += 
-`<div class ="pokemon-card">
-                     
-                      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-                       <h3>#${pokemon.id} ${pokemon.name}</h3>
-                 </div>`;
-                 
 
+    
+    contentRef.innerHTML += `
+        <div class="pokemon-card" id="pokemon-${pokemon.id}">
+            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+            <h3>#${pokemon.id} ${pokemon.name}</h3>
+        </div>
+    `;
+
+   
+    setTimeout(() => {
+        const cardRef = document.getElementById(`pokemon-${pokemon.id}`);
+        if (cardRef) {
+            cardRef.addEventListener('click', () => {
+                toggleOverlay(pokemon);
+            });
+        }
+    }, 200); 
+}
+
+function toggleOverlay(pokemon) {
+    const overlayRef = document.getElementById('overlay');
+
+    if (pokemon) {
+        overlayRef.innerHTML = `
+            <div class="pokemon-card enlarged">
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                <h3>#${pokemon.id} ${pokemon.name}</h3>
+            </div>
+        `;
+        overlayRef.classList.remove('hide');
+    } else {
+        overlayRef.classList.add('hide');
+    }
 }
 
 document.getElementById('loadMoreBtn').addEventListener('click', () => {
